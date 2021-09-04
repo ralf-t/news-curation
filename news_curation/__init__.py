252e32967d6c563db4685681f07d69a67edf62eb
Iionsroar <kiseryota023@gmail.com>
2021-09-04 13:30:10 +0800

    tests

---
+++ news_curation/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+def create_app(test_config=None):
+	pass
+
+def register_extensions(app):
+	pass
+
+def register_blueprints(app):
+	pass
+
+def register_commands(app):
+	pass
