7d621a3e57f727014e9080fba457a9b17c530969
Iionsroar <kiseryota023@gmail.com>
2021-10-05 20:59:05 +0800

    fixed active nav link; shelved special character validators

--- news_curation/user/forms.py
+++ news_curation/user/forms.py
@@ -5,15 +5,15 @@ from news_curation.user.models import User
 
 class RegistrationForm(FlaskForm):
 	first_name = StringField('First Name', validators=[DataRequired(), 
-													Length(2, 15, 'Firstname must be between 2 and 15 characters long.'),
-													Regexp("^[a-zA-Z\-\']+$", message="Firstname must not include special characters.")])
+													Length(2, 15, 'Firstname must be between 2 and 15 characters long.')])
+													# Regexp("^[a-zA-Z\-\']+$", message="Firstname must not include special characters.")],)
 	last_name = StringField('Last Name', validators=[DataRequired(), 
-													Length(2, 15, 'Lastname must be between 2 and 15 characters long.'),
-													Regexp("^[a-zA-Z\-\']+$", message="Lastname must not include special characters.")])
+													Length(2, 15, 'Lastname must be between 2 and 15 characters long.')])
+													# Regexp("^[a-zA-Z\-\']+$", message="Lastname must not include special characters.")])
 	username = StringField('Username', validators=[DataRequired(), 
 												# ST: https://security.stackexchange.com/questions/46875/why-is-there-a-minimum-username-length
-												Length(6, 20, 'Username must be between 6 and 20 characters long.'),
-												Regexp('^[a-zA-Z0-9]+$', message="Username must not include special characters.")])
+												Length(2, 20, 'Username must be between 2 and 20 characters long.')])
+												# Regexp('^[a-zA-Z0-9]+$', message="Username must not include special characters.")])
 	email = StringField('Email', validators=[DataRequired(), Email()])
 
 	password = PasswordField('Password', validators=[DataRequired()])
