cc796f1a41dfa7b38393bd1a20c03171baf1ef94
ralf-t <tabo.ralfmartin@ue.edu.ph>
2021-10-12 22:59:25 +0800

    post read route

--- news_curation/post/routes.py
+++ news_curation/post/routes.py
@@ -1,5 +1,15 @@
+from flask_login import login_required
+
 from news_curation.post import bp
+from news_curation.post.models import Post
 
 @bp.route("/")
 def home():
     return "post home"
+
+@bp.route("/<int:post_id>")
+@login_required
+def read(post_id):
+    post = Post.query.get_or_404(post_id)
+
+    return post.content
