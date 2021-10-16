6c02d6410d43e147b3b2fcdbaf35dee95c8922e4
ralf-t <tabo.ralfmartin@ue.edu.ph>
2021-10-16 23:22:03 +0800

    stuff

--- news_curation/comment/forms.py
+++ news_curation/comment/forms.py
@@ -1,1 +1,13 @@
-# fake news classifier
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length, ValidationError
+import random
+
+class CommentForm(FlaskForm):
+	content = StringField('Username', validators=[DataRequired(), Length(min=1)])
+	submit = SubmitField('Submit')
+
+	# validation for duplicate username/email
+	def validate_content(self, comment):
+        if random.randint(0, 10) >= 5:
+            raise ValidationError("Your comment does not seem to be reliable.")
--- news_curation/post/routes.py
+++ news_curation/post/routes.py
@@ -3,13 +3,20 @@ from flask_login import login_required
 from news_curation.post import bp
 from news_curation.post.models import Post
 
+from news_curation.comment.forms import CommentForm
+
 @bp.route("/")
 def home():
     return "post home"
 
-@bp.route("/<int:post_id>")
+@bp.route("/<int:post_id>", methods=["POST","GET"])
 @login_required
 def read(post_id):
     post = Post.query.get_or_404(post_id)
+
+    form = CommentForm()
+
+    # if form.validate_on_submit():
+    #     pass
 
     return post.content
