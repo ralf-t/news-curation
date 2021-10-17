f707443bf1514e0e19fea8bee6d1dc2abbd989c7
ralf-t <tabo.ralfmartin@ue.edu.ph>
2021-10-17 22:14:16 +0800

    syntax stuff

--- news_curation/comment/forms.py
+++ news_curation/comment/forms.py
@@ -9,5 +9,5 @@ class CommentForm(FlaskForm):
 
 	# validation for duplicate username/email
 	def validate_content(self, comment):
-        if random.randint(0, 10) >= 5:
-            raise ValidationError("Your comment does not seem to be reliable.")
+		if random.randint(0, 10) >= 5:
+			raise ValidationError("Your comment does not seem to be reliable.")
