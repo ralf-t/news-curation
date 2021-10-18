ffbbbb55d96f6c01258b007d16747f1264db2480
Iionsroar <kiseryota023@gmail.com>
2021-10-18 18:29:25 +0800

    interface requirements fix

--- news_curation/post/forms.py
+++ news_curation/post/forms.py
@@ -4,8 +4,8 @@ from wtforms.validators import DataRequired
 
 
 class PostForm(FlaskForm):
-	title = StringField('Title', validators=[DataRequired()])
-	content = TextAreaField('Content', validators=[DataRequired()])
+	title = StringField('Title', validators=[DataRequired()], render_kw={'placeholder': 'Title*'})
+	content = TextAreaField('Content', validators=[DataRequired()], render_kw={'placeholder': 'Content*'})
 	topics = SelectField('Topics', coerce=int, validators=[DataRequired()]) #dropdown options are defined in routes.py
 
 	add_topic = SubmitField('Add Tag')
